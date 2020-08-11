34d7038dcc463880f8fed5f291d3f6e5114fcee7
Fumbuka Gwesa <69437636+gwesa@users.noreply.github.com>
2020-08-11 07:51:10 +0300

    add files of hardware project

---
+++ dashbord.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles/project.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashbord</title>
+</head>
+    <div class="wrapper">
+      <div class="sidebar">
+          <h2>Dashbord</h2>
+          <ul>
+          <li><a href="dashbord.php"><i class="fa fa-home" ></i>Home</a></li>
+              <li><a href="products.php"><i class="fa fa-search"></i>Product</a></li>
+              <li><a href="my cart.php"><i class="fa fa-shopping-cart" >My cart</i></li>
+              <li><a href="#"><i class="fas fa-map"></i>Location</a></li>
+              <li><a href="logout.php">Logout</a></li>
+          </ul>
+      </div>
+     
+    
+<body>
+<div class="main_content" style="float:right;">
+          <div class="header">Welcome!! To our shopping system<br>Enjoy our service you can buy your products of your choise</div> 
+    <div>
+        <table>
+            <tr>
+                <td><img src="download1.jpeg" height="200" widith="300"></td>
+                <td><img src="download.jpeg" height="200" widith="300"></td>
+                <td><img src="images4.jpeg" height="200" widith="300"></td>
+            </tr>
+            <tr>
+                <td><img src="images5.jpeg" height="200" widith="300"></td>
+                <td><img src="images6.jpeg" height="200" widith="300"></td>
+                <td><img src="images7.jpeg" height="200" widith="300"></td>
+            </tr>
+            <tr>
+                <td><img src="images8.jpeg" height="200" widith="300"></td>
+                <td><img src="images9.jpeg" height="200" widith="300"></td>
+                <td><img src="download.jpeg" height="200" widith="300"></td>
+            </tr>
+        </table>
+
+    </div>
+    </div>
+    </div>
+    
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>shopping</title>
+    <style>
+    body{
+        background-image:url(IMAGE.jpg);
+                justify-content:center;
+                height: 100vh;
+                color:greenyellow;
+            }
+            *{
+                font-family:sans-serif;
+                box-sizing:border-box;
+            }
+          
+            h1{
+                text-align:left;
+                margin-bottom: 40px;
+                color:greenyellow;
+            }
+           
+            label{
+                color: #888;
+                font-size: 18px;
+                padding: 10px;
+            }
+            footer{
+                background-position:below;
+                text-align:right;
+                align: bottom;
+                
+            }
+           
+        </style>
+</head>
+
+<body>
+<div class="header">
+<h1>HARDWARE SHOPING SYSTEM</h1>
+</div>
+<div align="left">
+    <tr> <ol>
+        <th><li><a href="login.html">HOME</a></li>
+        <li><a href="">PRODUCT</a></li>
+        <li><a href="">SALES</a></li>
+        <li><a href="">PURCHASE</a></li></th>
+        </ol>
+     </tr>
+     </div>
+
+<a href="logout.php">Logout</a>
+
+        <div class="footer">
+         <p>          
+             For More Information:
+             +255 763 901 262,
+             +255 621 698 480</p>
+                 <p>
+                    copyright@2020,  
+                 family.company.tz</p>
+        </div>
+    
+</body>
+</html>
